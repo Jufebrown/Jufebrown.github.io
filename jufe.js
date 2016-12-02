65b7209d908b91b5179ac5ae98247ebc01d3a09d
Jufebrown <jufebrown@gmail.com>
2016-12-02 12:52:45 -0600

    added scroll reveal and customized it

--- jufe.js
+++ jufe.js
@@ -7,3 +7,16 @@ $('.navbar li a').click(function(event) {
     $($(this).attr('href'))[0].scrollIntoView();
     scrollBy(0, -offset);
 });
+
+var customReveal = {
+  origin: 'left',
+  delay    : 200,
+  duration: 1000,
+  distance : '600px',
+  easing   : 'ease-in-out',
+  scale    : 1.1
+};
+
+window.sr = ScrollReveal();
+sr.reveal('.projects-section', customReveal);
+sr.reveal('.contact-wrapper', customReveal);
