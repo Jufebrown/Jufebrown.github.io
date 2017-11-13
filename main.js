44571879dae83f7dcfd602a5f1673c30a9a59a3e
Jufebrown <jufebrown@gmail.com>
2017-11-13 10:53:42 -0600

    browserifyed js files

---
+++ main.js
@@ -0,0 +1,6 @@
+const jquery = require('./node_modules/jquery/dist/jquery.min.js');
+const bootstrap = require('./node_modules/bootstrap/dist/js/bootstrap.min.js');
+const angular = require('./node_modules/angular/angular.min.js');
+const angularRoute = require('./node_modules/angular-route/angular-route.min.js');
+
+const app = require('./app/app.js');
