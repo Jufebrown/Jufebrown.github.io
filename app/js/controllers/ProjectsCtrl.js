d76364b4bbd5d52c69653ec700c21c7962d11b16
Jufebrown <jufebrown@gmail.com>
2017-11-21 11:05:25 -0600

    removed unnecessary carousel code

--- app/js/controllers/ProjectsCtrl.js
+++ app/js/controllers/ProjectsCtrl.js
@@ -1,6 +1,7 @@
 'use strict';
 
 module.exports = function($scope) {
+
   $scope.myInterval = 5000;
   $scope.noWrapSlides = false;
   $scope.active = 0;
@@ -21,46 +22,8 @@ module.exports = function($scope) {
     });
   };
 
-  $scope.randomize = function() {
-    var indexes = generateIndexesArray();
-    assignNewIndexesToSlides(indexes);
-  };
-
   for (var i = 0; i < 4; i++) {
     $scope.addSlide();
   }
 
-  // Randomize logic below
-
-  function assignNewIndexesToSlides(indexes) {
-    for (var i = 0, l = slides.length; i < l; i++) {
-      slides[i].id = indexes.pop();
-    }
-  }
-
-  function generateIndexesArray() {
-    var indexes = [];
-    for (var i = 0; i < currIndex; ++i) {
-      indexes[i] = i;
-    }
-    return shuffle(indexes);
-  }
-
-  // http://stackoverflow.com/questions/962802#962890
-  function shuffle(array) {
-    var tmp,
-      current,
-      top = array.length;
-
-    if (top) {
-      while (--top) {
-        current = Math.floor(Math.random() * (top + 1));
-        tmp = array[current];
-        array[current] = array[top];
-        array[top] = tmp;
-      }
-    }
-
-    return array;
-  }
 };
