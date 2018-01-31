901608223a379f1053633a4890f4996471019d0c
Jufebrown <jufebrown@gmail.com>
2018-01-31 11:04:11 -0600

    animated squares on home page.

--- app/js/controllers/HomeCtrl.js
+++ app/js/controllers/HomeCtrl.js
@@ -3,21 +3,29 @@
 module.exports = function($scope, homeFactory) {
   document.querySelector('.home-nav').focus();
 
-  // $scope.gridCells = homeFactory.generateGridCells();
-
-  let canvas = document.getElementById('homeCanvas');
+  const canvas = document.getElementById('homeCanvas');
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight * 0.8;
   const ctx = canvas.getContext('2d');
-  let raf;
 
   let square = {
     x: 10,
     y: 10,
-    vx: 3,
-    vy: 5,
+    vx: 5,
+    vy: 2,
     draw: function() {
       ctx.fillStyle = 'rgb(200, 0, 0)';
+      ctx.fillRect(this.x, this.y, 250, 250);
+    }
+  };
+
+  let square2 = {
+    x: 100,
+    y: 100,
+    vx: 3,
+    vy: 1,
+    draw: function() {
+      ctx.fillStyle = 'rgb(0, 200, 0)';
       ctx.fillRect(this.x, this.y, 250, 250);
     }
   };
@@ -25,16 +33,20 @@ module.exports = function($scope, homeFactory) {
   $scope.draw = function() {
     if (canvas.getContext) {
       ctx.fillStyle = 'rgba(255, 255, 255, 0.3)';
-      ctx.fillRect(0, 0, canvas.width, canvas.height);
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
       square.draw();
       square.x += square.vx;
       square.y += square.vy;
-      raf = window.requestAnimationFrame($scope.draw);
+      square2.draw();
+      square2.x += square2.vx;
+      square2.y += square2.vy;
+      // window.requestAnimationFrame($scope.draw);
     } else {
       /* TODO: put something here for people using old browsers */
       console.log('canvas not supported');
     }
   };
 
-  $scope.draw();
+  window.setInterval($scope.draw, 5);
+  // $scope.draw();
 };
